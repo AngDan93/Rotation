9452ee6fcb0363af091f22518bacc189e947d8a3
Angel Daniel Munguia Mendoza <angdan93@gmail.com>
2021-07-28 23:35:53 -0500

    Agregar archivos de proyecto.

---
+++ Rotation/Rotation.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Rotation
+{
+    partial class Rotation
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.axisX = new System.Windows.Forms.NumericUpDown();
+            this.axisY = new System.Windows.Forms.NumericUpDown();
+            this.axisZ = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axisX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axisY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axisZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axisX
+            // 
+            this.axisX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.axisX.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.axisX.Location = new System.Drawing.Point(46, 30);
+            this.axisX.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.axisX.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.axisX.Name = "axisX";
+            this.axisX.Size = new System.Drawing.Size(120, 29);
+            this.axisX.TabIndex = 0;
+            this.axisX.ValueChanged += new System.EventHandler(this.axisX_ValueChanged);
+            // 
+            // axisY
+            // 
+            this.axisY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.axisY.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.axisY.Location = new System.Drawing.Point(46, 65);
+            this.axisY.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.axisY.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.axisY.Name = "axisY";
+            this.axisY.Size = new System.Drawing.Size(120, 29);
+            this.axisY.TabIndex = 1;
+            this.axisY.ValueChanged += new System.EventHandler(this.axisY_ValueChanged);
+            // 
+            // axisZ
+            // 
+            this.axisZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.axisZ.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.axisZ.Location = new System.Drawing.Point(46, 100);
+            this.axisZ.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.axisZ.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.axisZ.Name = "axisZ";
+            this.axisZ.Size = new System.Drawing.Size(120, 29);
+            this.axisZ.TabIndex = 2;
+            this.axisZ.ValueChanged += new System.EventHandler(this.axisZ_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Z";
+            // 
+            // Rotation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 154);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axisZ);
+            this.Controls.Add(this.axisY);
+            this.Controls.Add(this.axisX);
+            this.Name = "Rotation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rotation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axisX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axisY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axisZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown axisX;
+        private System.Windows.Forms.NumericUpDown axisY;
+        private System.Windows.Forms.NumericUpDown axisZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Rotation/Rotation.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Math;
+using static System.Convert;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace Rotation
+{
+    public partial class Rotation : Form
+    {
+        public Rotation()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            swApp = new SldWorks();
+            if(swModel == null)
+            {
+                swModel = (ModelDoc2)swApp.ActiveDoc;
+
+            }
+        }
+
+        private void rotate()
+        {
+            
+
+            double A = ToDouble(axisX.Value) * PI / 180;
+            double B = ToDouble(axisY.Value) * PI / 180;
+            double C = ToDouble(axisZ.Value) * PI / 180;
+
+
+            //Rotate in XYZ
+            double[] m = new double[9]
+            {
+                Cos(B)*Cos(C) , -Cos(A)*Sin(C) + Sin(A)*Sin(B)*Cos(C) , Sin(A)*Sin(C) + Cos(A)*Sin(B)*Cos(C),
+                Cos(B)*Sin(C) ,  Cos(A)*Cos(C) + Sin(A)*Sin(B)*Sin(C) ,-Sin(A)*Cos(C) + Cos(A)*Sin(B)*Sin(C),
+                -Sin(B)       ,  Sin(A)*Cos(B)                        , Cos(A)*Cos(B)
+            };
+
+
+            ////Only Rotate in X
+            /////double angle = ToDouble(axisX.Value) * PI / 180;
+            //double[] m = new double[9]
+            //{
+            //    1, 0         , 0,
+            //    0, Cos(angle), -Sin(angle),
+            //    0, Sin(angle), Cos(angle)
+            //};
+
+            ////Only Rotate in Y
+            //double angle = ToDouble(axisY.Value) * PI / 180;
+            //double[] m = new double[9]
+            //{
+            //    Cos(angle) , 0, Sin(angle),
+            //    0          , 1, 0,
+            //    -Sin(angle), 0, Cos(angle)
+            //};
+
+            ////Only Rotate in Z
+            //double angle = ToDouble(axisZ.Value) * PI / 180;
+            //double[] m = new double[9]
+            //{
+            //    Cos(angle),-Sin(angle), 0,
+            //    Sin(angle), Cos(angle), 0,
+            //    0         , 0         , 1
+            //};
+
+
+            try
+            {
+               
+                if (swApp != null)
+                {
+                   
+                    if (swModel != null)
+                    {
+                        if (swModel.GetType() == (int)swDocumentTypes_e.swDocASSEMBLY)
+                        {
+                            
+                            if (myComp == null)
+                            {
+                                selectComp();
+                            }
+                            
+
+                            if (myComp != null)
+                            {
+                                object XformIn = new double[16]
+                                {
+                                        m[0],  m[1],  m[2],
+                                        m[3],  m[4],  m[5],
+                                        m[6],  m[7],  m[8],
+                                        0   ,  0   ,  0   ,
+                                        1   ,  0   ,  0   ,
+                                        0.0
+                                };
+                                MathUtility swMathUtility = (MathUtility)swApp.GetMathUtility();
+                                MathTransform transform = (MathTransform)swMathUtility.CreateTransform(XformIn);
+                                bool sts = myComp.SetTransformAndSolve2(transform);
+                                if (sts)
+                                     swModel.EditRebuild3();
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please, open an assembly", "Component selector", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                        }
+
+                    }
+                    
+                }
+                
+
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        public SldWorks swApp;
+        public ModelDoc2 swModel;
+        Component2 myComp;
+
+        private void axisX_ValueChanged(object sender, EventArgs e)
+        {
+            rotate();
+        }
+
+        private void axisY_ValueChanged(object sender, EventArgs e)
+        {
+            rotate();
+        }
+
+        private void axisZ_ValueChanged(object sender, EventArgs e)
+        {
+            rotate();
+        }
+        private void selectComp()
+        {
+            SelectionMgr selMgr = (SelectionMgr)swModel.SelectionManager;
+            MessageBox.Show("Please, select a component", "Component selector", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            if (selMgr.GetSelectedObjectCount2(Mark: -1) != 0)
+            {
+
+                if (selMgr.GetSelectedObjectType3(1, -1) != (int)swSelectType_e.swSelCOMPONENTS)
+                {
+                    if (selMgr.GetSelectedObjectType3(1, -1) == (int)swSelectType_e.swSelFACES)
+                    {
+                        Entity swEnt = (Entity)selMgr.GetSelectedObject6(1, -1);
+                        myComp = (Component2)swEnt.GetComponent();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No selected component");
+                    }
+
+                }
+                else
+                {
+                    myComp = selMgr.GetSelectedObject6(1, -1);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("No selected component");
+            }
+
+        }
+    }
+
+}
